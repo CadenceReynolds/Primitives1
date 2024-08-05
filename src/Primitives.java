8ec58074de2fbd5529c6591da5decb856a73d5fc
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-05 12:56:18 -0400

    Update

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Primitives {
+import java.awt.image.ShortLookupTable;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        int myInt = 7; //32-bit integer or a 4 byte integer
+        System.out.println(myInt);
+        short myShort = 8;
+        System.out.println(myShort);
+        byte myByte = 9;
+        System.out.println(myByte);
+        long myLong = 2000000000000000000L;
+        System.out.println(myLong);
+
+
+        String myString = "Hello World";
+        System.out.println(myString);
+
+        char myChar = 'a';
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        Integer myInt2 = 7; //32-bit integer or a 4 byte integer
+        System.out.println(myInt2);
+        Short myShort2 = 8;
+        System.out.println(myShort2);
+        Byte myByte2 = 9;
+        System.out.println(myByte2);
+        Long myLong2 = 2000000000000000000L;
+        System.out.println(myLong2);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+
+
+    }
+
+
 }
